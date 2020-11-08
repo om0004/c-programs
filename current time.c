0fc4e700ae3521ae08b2c6d07863bf1902c184d0
om <60003596+om0004@users.noreply.github.com>
2020-11-08 19:39:44 +0530

    Add files via upload

---
+++ current time.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<time.h>
+
+int main()
+{
+;
+
+    time_t t;   // not a primitive datatype
+    time(&t);
+
+    printf("\nThis program has been written at (date and time): %s", ctime(&t));
+
+
+    return 0;
+}
